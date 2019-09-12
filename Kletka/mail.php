9d8b0fb6f1e496000d752d80d9e007b7a3147f5f
DriIIer <49284064+DriIIer@users.noreply.github.com>
2019-09-12 23:35:15 +0300

    Add files via upload

---
+++ Kletka/mail.php
@@ -0,0 +1,14 @@
+<?php 
+	
+	$recepient = 'danilnikitenko93@gmail.com';
+	$siteName = 'Клетка';
+
+	$name = trim($_POST['name']);
+	$phone = trim($_POST['phone']);
+	$question = trim($_POST['question']);
+	$message = 'Имя: $name \nНомер телефона: $phone \nБронь или вопрос: $question';
+
+	$pagetittle = 'Заявка с сайта \'$siteName\'';
+	mail($recepient, $pagetittle, $message, 'Content-type: text-plain; charset=\'utf-8\'\n From: $recepient');
+
+ ?>
